adf35d50a8ceef648c9562e4a618863bfbe50a24
mu88 <4560672+mu88@users.noreply.github.com>
2023-07-05 18:57:43 +0200

    build: allow installation of PowerShell for both arm64 and amd64

---
+++ src/ScreenshotCreator.Api/install-powershell-amd64.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+
+apt update && apt install -y curl gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --yes --dearmor --output /usr/share/keyrings/microsoft.gpg
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
+apt update && apt install -y powershell
---
+++ src/ScreenshotCreator.Api/install-powershell-arm64.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+apt-get update
+apt-get install -y wget libssl1.1 libunwind8
+mkdir -p /opt/microsoft/powershell/7
+wget -O /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.6/powershell-7.2.6-linux-arm64.tar.gz
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+chmod +x /opt/microsoft/powershell/7/pwsh
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+rm /tmp/powershell.tar.gz
