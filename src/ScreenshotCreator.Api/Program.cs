0318818a578d8ba5c8d036ef7b04f03f230ed8ea
mu88 <4560672+mu88@users.noreply.github.com>
2023-07-07 16:04:40 +0200

    chore: move secrets to file outside VCS

--- src/ScreenshotCreator.Api/Program.cs
+++ src/ScreenshotCreator.Api/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("appsettings.secret.json", true);
 builder.Services.Configure<ScreenshotOptions>(builder.Configuration.GetSection(ScreenshotOptions.SectionName));
 builder.Services.AddSingleton<Creator>();
 builder.Services.AddSingleton<ImageProcessor>();
