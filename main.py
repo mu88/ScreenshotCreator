915a3099a14acf9390f67dde48f6ca164f89a630
mu88 <4560672+mu88@users.noreply.github.com>
2023-07-08 18:02:52 +0200

    chore: add Python script for Waveshare ePaper display

---
+++ main.py
@@ -0,0 +1,321 @@
+﻿# see https://github.com/waveshareteam/Pico_ePaper_Code/blob/main/python/Pico-ePaper-7.5-B.py
+
+# *****************************************************************************
+# * | File        :	  Pico_ePaper-7.5-B.py
+# * | Author      :   Waveshare team
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * | This version:   V1.0
+# * | Date        :   2021-05-27
+# # | Info        :   python demo
+# -----------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import framebuf
+import gc
+from machine import Pin, SPI
+import network
+import time
+import urequests
+import utime
+
+# Display resolution
+EPD_WIDTH       = 800
+EPD_HEIGHT      = 480
+
+RST_PIN         = 12
+DC_PIN          = 8
+CS_PIN          = 9
+BUSY_PIN        = 13
+
+class EPD_7in5_B:
+    def __init__(self):
+        self.reset_pin = Pin(RST_PIN, Pin.OUT)
+        
+        self.busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
+        self.cs_pin = Pin(CS_PIN, Pin.OUT)
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+        
+        self.spi = SPI(1)
+        self.spi.init(baudrate=4000_000)
+        self.dc_pin = Pin(DC_PIN, Pin.OUT)
+        
+        self.buffer_black = bytearray(self.height * self.width // 8)
+        self.buffer_red = bytearray(self.height * self.width // 8)
+        self.imageblack = framebuf.FrameBuffer(self.buffer_black, self.width, self.height, framebuf.MONO_HLSB)
+        self.imagered = framebuf.FrameBuffer(self.buffer_red, self.width, self.height, framebuf.MONO_HLSB)
+        self.init()
+
+    def digital_write(self, pin, value):
+        pin.value(value)
+
+    def digital_read(self, pin):
+        return pin.value()
+
+    def delay_ms(self, delaytime):
+        utime.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.spi.write(bytearray(data))
+
+    def module_exit(self):
+        self.digital_write(self.reset_pin, 0)
+
+    # Hardware reset
+    def reset(self):
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(200) 
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(200)   
+
+    def send_command(self, command):
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+        
+    def send_data1(self, buf):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi.write(bytearray(buf))
+        self.digital_write(self.cs_pin, 1)
+
+    def WaitUntilIdle(self):
+        print("e-Paper busy")
+        while(self.digital_read(self.busy_pin) == 0):   # Wait until the busy_pin goes LOW
+            self.delay_ms(20)
+        self.delay_ms(20) 
+        print("e-Paper busy release")  
+
+    def TurnOnDisplay(self):
+        self.send_command(0x12) # DISPLAY REFRESH
+        self.delay_ms(100)      #!!!The delay here is necessary, 200uS at least!!!
+        self.WaitUntilIdle()
+        
+    def init(self):
+        # EPD hardware init start     
+        self.reset()
+        
+        self.send_command(0x06)     # btst
+        self.send_data(0x17)
+        self.send_data(0x17)
+        self.send_data(0x28)        # If an exception is displayed, try using 0x38
+        self.send_data(0x17)
+        
+#         self.send_command(0x01)  # POWER SETTING
+#         self.send_data(0x07)
+#         self.send_data(0x07)     # VGH=20V,VGL=-20V
+#         self.send_data(0x3f)     # VDH=15V
+#         self.send_data(0x3f)     # VDL=-15V
+        
+        self.send_command(0x04)  # POWER ON
+        self.delay_ms(100)
+        self.WaitUntilIdle()
+
+        self.send_command(0X00)   # PANNEL SETTING
+        self.send_data(0x0F)      # KW-3f   KWR-2F	BWROTP 0f	BWOTP 1f
+
+        self.send_command(0x61)     # tres
+        self.send_data(0x03)     # source 800
+        self.send_data(0x20)
+        self.send_data(0x01)     # gate 480
+        self.send_data(0xE0)
+
+        self.send_command(0X15)
+        self.send_data(0x00)
+
+        self.send_command(0X50)     # VCOM AND DATA INTERVAL SETTING
+        self.send_data(0x11)
+        self.send_data(0x07)
+
+        self.send_command(0X60)     # TCON SETTING
+        self.send_data(0x22)
+
+        self.send_command(0x65)     # Resolution setting
+        self.send_data(0x00)
+        self.send_data(0x00)     # 800*480
+        self.send_data(0x00)
+        self.send_data(0x00)
+        
+        return 0;
+
+    def Clear(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10)
+        for i in range(0, wide):
+            self.send_data1([0xff] * high)
+                
+        self.send_command(0x13) 
+        for i in range(0, wide):
+            self.send_data1([0x00] * high)
+                
+        self.TurnOnDisplay()
+        
+    def ClearRed(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10) 
+        for i in range(0, wide):
+            self.send_data1([0xff] * high)
+                
+        self.send_command(0x13) 
+        for i in range(0, wide):
+            self.send_data1([0xff] * high)
+                
+        self.TurnOnDisplay()
+        
+    def ClearBlack(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10) 
+        for i in range(0, wide):
+            self.send_data1([0x00] * high)
+                
+        self.send_command(0x13) 
+        for i in range(0, wide):
+            self.send_data1([0x00] * high)
+                
+        self.TurnOnDisplay()
+        
+    def display(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        # send black data
+        self.send_command(0x10) 
+        for i in range(0, wide):
+            self.send_data1(self.buffer_black[(i * high) : ((i+1) * high)])
+            
+        # send red data
+        self.send_command(0x13) 
+        for i in range(0, wide):
+            self.send_data1(self.buffer_red[(i * high) : ((i+1) * high)])
+            
+        self.TurnOnDisplay()
+
+
+    def sleep(self):
+        self.send_command(0x02) # power off
+        self.WaitUntilIdle()
+        self.send_command(0x07) # deep sleep
+        self.send_data(0xa5)
+
+def connect_wifi():
+    pin = Pin("LED", Pin.OUT)
+    pin.off()
+
+    # waiting some time between connecting and checking the status is crucial for a reliable WiFi connection
+    wifi = network.WLAN(network.STA_IF)
+    wifi.active(True)
+    time.sleep(2)
+    wifi.connect('<<SSID>>', '<<Password>>')
+    time.sleep(2)
+
+    max_wait = 10
+    while max_wait > 0:
+        if wifi.status() < 0 or wifi.status() >= 3:
+            break
+        max_wait -= 1
+        time.sleep(1)
+
+    if wifi.status() != 3:
+        raise RuntimeError('network connection failed')
+    
+    wifi = None
+
+if __name__=='__main__':
+    gc.enable()
+
+    pin = Pin("LED", Pin.OUT)
+    pin.off()
+
+    epd = EPD_7in5_B()
+    epd.Clear()
+
+    sleep_in_seconds = 90
+    empty_buffer = bytearray(0)
+
+    try:
+        connect_wifi()
+
+        while(True):
+            gc.collect()
+            response = urequests.get('http://<<Server>>/screenshotCreator/latestImage?blackAndWhite=true&asWaveshareBytes=true&addWaveshareInstructions=true')
+
+            if response.status_code == 200:
+                update_screen = response.headers.get('waveshare-update-screen', 'True') == 'True'
+                sleep_in_seconds = int(response.headers.get('waveshare-sleep-between-updates', '90'))
+
+                if update_screen:
+                    last_modified = response.headers.get('waveshare-last-modified-local-time', 'xx:xx')
+
+                    epd.init() # explicitly wake up display from deep sleep
+
+                    # assign black and red portion
+                    epd.buffer_black = response.content
+                    epd.imagered.fill(0x00)
+                    epd.imagered.text(last_modified, 550, 460, 0xff)
+
+                    # display and go to deep sleep
+                    epd.display()
+                    epd.sleep()
+
+                    # do not leak memory
+                    epd.buffer_black = empty_buffer
+                    update_screen = None
+                    last_modified = None
+                    response = None
+
+            time.sleep(sleep_in_seconds)
+    except Exception as e:
+        print(e)
+        pin = Pin("LED", Pin.OUT)
+        pin.on()
+        raise e
