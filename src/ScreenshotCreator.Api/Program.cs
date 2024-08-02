4be013cd041236e0c419de3f64e1acb476465325
mu88 <4560672+mu88@users.noreply.github.com>
2024-08-02 18:06:39 +0200

    feat: replace OpenTelemetry and multi-manifest Docker image logic with NuGet package mu88.Shared

--- src/ScreenshotCreator.Api/Program.cs
+++ src/ScreenshotCreator.Api/Program.cs
@@ -1,27 +1,24 @@
 using Microsoft.Extensions.Options;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
+using mu88.Shared.OpenTelemetry;
 using ScreenshotCreator.Api;
 using ScreenshotCreator.Logic;
 using Creator = ScreenshotCreator.Logic.ScreenshotCreator;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureOpenTelemetry(builder);
+builder.ConfigureOpenTelemetry("ScreenshotCreator");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Configuration
-    .AddJsonFile("appsettings.secret.json", true)
-    .AddKeyPerFile("/run/secrets", true);
+       .AddJsonFile("appsettings.secret.json", true)
+       .AddKeyPerFile("/run/secrets", true);
 builder.Services
-    .AddOptions<ScreenshotOptions>()
-    .Bind(builder.Configuration.GetSection(ScreenshotOptions.SectionName))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
+       .AddOptions<ScreenshotOptions>()
+       .Bind(builder.Configuration.GetSection(ScreenshotOptions.SectionName))
+       .ValidateDataAnnotations()
+       .ValidateOnStart();
 builder.Services.AddSingleton<IScreenshotCreator, Creator>();
 builder.Services.AddSingleton<IPlaywrightHelper, PlaywrightHelper>();
 builder.Services.AddSingleton<ImageProcessor>();
@@ -39,22 +36,22 @@ app.UsePathBase("/screenshotCreator");
 
 app.MapGet("latestImage", ReturnImageOrNotFoundAsync);
 app.MapGet("createImageNow",
-           async (HttpContext httpContext, ImageProcessor imageProcessor, IScreenshotCreator creator, IOptions<ScreenshotOptions> options) =>
-           {
-               await creator.CreateScreenshotAsync(options.Value.Width, options.Value.Height);
-               return await ReturnImageOrNotFoundAsync(httpContext, imageProcessor, options);
-           });
+    async (HttpContext httpContext, ImageProcessor imageProcessor, IScreenshotCreator creator, IOptions<ScreenshotOptions> options) =>
+    {
+        await creator.CreateScreenshotAsync(options.Value.Width, options.Value.Height);
+        return await ReturnImageOrNotFoundAsync(httpContext, imageProcessor, options);
+    });
 app.MapGet("createImageWithSizeNow",
-           async (uint width,
-                  uint height,
-                  HttpContext httpContext,
-                  ImageProcessor imageProcessor,
-                  IOptions<ScreenshotOptions> options,
-                  IScreenshotCreator creator) =>
-           {
-               await creator.CreateScreenshotAsync(width, height);
-               return await ReturnImageOrNotFoundAsync(httpContext, imageProcessor, options);
-           });
+    async (uint width,
+           uint height,
+           HttpContext httpContext,
+           ImageProcessor imageProcessor,
+           IOptions<ScreenshotOptions> options,
+           IScreenshotCreator creator) =>
+    {
+        await creator.CreateScreenshotAsync(width, height);
+        return await ReturnImageOrNotFoundAsync(httpContext, imageProcessor, options);
+    });
 
 await app.RunAsync();
 
@@ -82,33 +79,4 @@ async Task<IResult> ReturnImageOrNotFoundAsync(HttpContext httpContext,
     }
 
     return result;
-}
-
-static void ConfigureOpenTelemetry(IHostApplicationBuilder builder)
-{
-    builder.Logging.AddOpenTelemetry(logging =>
-    {
-        logging.IncludeFormattedMessage = true;
-        logging.IncludeScopes = true;
-    });
-
-    builder.Services
-        .AddOpenTelemetry()
-        .ConfigureResource(c => c.AddService("ScreenshotCreator"))
-        .WithMetrics(metrics =>
-        {
-            metrics
-                .AddAspNetCoreInstrumentation()
-                .AddRuntimeInstrumentation();
-        })
-        .WithTracing(tracing =>
-        {
-            tracing.AddAspNetCoreInstrumentation();
-        });
-
-    var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
-    if (useOtlpExporter)
-    {
-        builder.Services.AddOpenTelemetry().UseOtlpExporter();
-    }
 }
